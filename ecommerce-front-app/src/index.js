da9ecca2d542f2159b4b6d11bc34f1de7216564f
ahsan7266 <74853314+ahsan7266@users.noreply.github.com>
2023-05-07 10:54:13 -0700

    done

--- ecommerce-front-app/src/index.js
+++ ecommerce-front-app/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './assets/css/vendor/vendor.min.css';
-import './assets/css/plugins/plugins.min.css';
-import './assets/css/style.min.css';
-import './assets/js/vendor/jquery-3.5.1.min';
-import './assets/js/vendor/vendor.min.js';
-import './assets/js/plugins/plugins.min.js';
-import './assets/js/main.js';
+// import './assets/css/vendor/vendor.min.css';
+// import './assets/css/plugins/plugins.min.css';
+// import './assets/css/style.min.css';
+// import './assets/js/vendor/jquery-3.5.1.min';
+// import './assets/js/vendor/vendor.min.js';
+// import './assets/js/plugins/plugins.min.js';
+// import './assets/js/main.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
